6ab2a6a9c26b8057fc6002c27021016ecbdd915c
saad <saadneosoft@gmail.com>
2023-07-18 12:00:36 +0530

    Initial Commit

---
+++ pracice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pracice
+//
+//  Created by Neosoft on 18/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
